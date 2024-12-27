21dee3db2a6feed2beb61f3a689511a391c7b6a8
vishal <vishal.93165095@gmail.com>
2024-12-27 14:45:14 +0530

    changed db configuration

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,35 @@
+// const mongoosse = require('mongoose');
+// const config = require('config'); // config module
+// const debug = require('debug')('development:mongoose'); // debug module
+
+
+
+// mongoosse.connect(`${config.get("MONGODB_URI")}/CrownCarry`)
+// .then(() => {
+//     debug("Connected"); 
+// })
+// .catch((err)=>{
+//     debug(err);
+// })
+
+// module.exports = mongoosse.connection;
 const mongoosse = require('mongoose');
+require('dotenv').config();
 const config = require('config'); // config module
 const debug = require('debug')('development:mongoose'); // debug module
 
 
-mongoosse.connect(`${config.get("MONGODB_URI")}/CrownCarry`)
+
+mongoosse.connect(`${process.env.MONGODB_URI}`, {
+     dbName: "CrownCarry",
+    connectTimeoutMS: 30000, // Set timeout to 30 seconds (30,000 milliseconds)
+})
 .then(() => {
-    debug("Connected"); 
+    debug("Connected");
 })
-.catch((err)=>{
-    debug(err);
-})
+.catch((err) => {
+    debug("Connection error:", err);
+});
+
 
 module.exports = mongoosse.connection;
